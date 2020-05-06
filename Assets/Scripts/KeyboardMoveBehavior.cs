496744acd863c26538d8ed307abd10e760c1ff38
Van Phelan <van.phelan@aie.edu>
2020-05-06 17:56:33 -0500

    Initial commit

---
+++ Assets/Scripts/KeyboardMoveBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMoveBehavior : MonoBehaviour
+{
+    /**
+     * A reference to the MoveBehavior.
+     **/
+    public MoveBehavior move;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Find the direction
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction.Normalize();
+
+        //Apply the direction
+        move.ApplyDirection(direction);
+    }
+}
---
+++ Assets/Scripts/MoveBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MoveBehavior : MonoBehaviour
+{
+    /**
+     * The magnitude of the movement vector.
+     **/
+    public float speed = 1.0f;
+
+    /**
+     * A reference to the CharacterController.
+     **/
+    public CharacterController characterController;
+
+    /**
+     * The direction of the movement vector.
+     **/
+    private Vector3 _direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Find the direction
+        _direction.Normalize();
+
+        //Move in the direction at the speed
+        characterController.Move(_direction * speed * Time.deltaTime);
+
+        //Reset the direction
+        _direction.x = 0;
+        _direction.y = 0;
+        _direction.z = 0;
+    }
+
+    /**
+     * Combine the input direction with the direction calculated so
+     * far this Update. This is reset each Update.
+     **/
+    public void ApplyDirection(Vector3 direction)
+    {
+        _direction += direction;
+    }
+}
