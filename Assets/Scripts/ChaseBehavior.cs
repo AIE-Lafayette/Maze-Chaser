f1fe5ee57eab9f54125c81d6396e05935bfec09d
Van Phelan <van.phelan@aie.edu>
2020-05-06 18:38:01 -0500

    Removed redundant behaviors and added pathfinding to enemies

--- Assets/Scripts/ChaseBehavior.cs
+++ Assets/Scripts/ChaseBehavior.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChaseBehavior : MonoBehaviour
 {
+    /**
+     * The transform this GameObject will attempt to reach.
+     **/
+    public Transform target;
+
+    private NavMeshAgent nav;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nav.SetDestination(target.position);
     }
 }
